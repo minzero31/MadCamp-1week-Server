ddb673767002598896582fae2481ee4d025b8824
minzero31 <77minyoung77@gmail.com>
2024-07-09 03:30:13 +0900

    "mypage ui first commit #10 "

--- app/src/main/java/com/example/flask_1/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/flask_1/ui/slideshow/SlideshowFragment.kt
@@ -4,31 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.example.flask_1.R
 import com.example.flask_1.databinding.FragmentSlideshowBinding
 
 class SlideshowFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel = ViewModelProvider(this).get(SlideshowViewModel::class.java)
-
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // View elements
+        val title: TextView = binding.title
+        val userIcon: ImageView = binding.userIcon
+        val userId: TextView = binding.userId
+        val userEmail: TextView = binding.userEmail
+        val btnViewMyQuestions: Button = binding.btnViewMyQuestions
+        val btnLogout: Button = binding.btnLogout
+
+        // Set any specific properties or listeners
+        btnViewMyQuestions.setOnClickListener {
+            // Handle "내가 만든 문제 보러 가기" button click
         }
+
+        btnLogout.setOnClickListener {
+            // Handle "로그아웃" button click
+        }
+
         return root
     }
 
